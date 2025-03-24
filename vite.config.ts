10a41d24e4984e4219353faa6ea3059f8666e82e
yoonbeeseo <yoon.tec.info@gmail.com>
2025-03-24 11:15:52 +0900

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/requirements-project/",
+  // base: "/requirements-project/",
 });
